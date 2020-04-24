a848c6ccaccebb70901c12f40a048ac6badd3bfc
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-04-23 17:25:53 -0700

    Initial Commit

---
+++ 2020-04-30-PartyPlanner-Huimin-Jiang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-04-30-PartyPlanner-Huimin-Jiang
+//
+//  Created by Huimin Jiang on 4/23/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
